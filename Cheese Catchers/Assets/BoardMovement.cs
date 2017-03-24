e034239d38628ac324c4b549f85dbafbcba923b2
Ebilkill <bartwijgers@hotmail.com>
2017-03-24 11:02:57 +0100

    Hamboard has max speed

---
+++ Cheese Catchers/Assets/BoardMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardMovement : MonoBehaviour {
+
+    public float maxSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody>().velocity *= (float)System.Math.Min(1, maxSpeed / System.Math.Sqrt(GetComponent<Rigidbody>().velocity.sqrMagnitude));
+	}
+}
