0a00e96f59979a86f37c4e601be06c87bb4ffea6
Esther <esther.oosterveer@hotmail.com>
2017-03-24 15:56:12 +0100

    add textures

---
+++ Cheese Catchers/Assets/Scripts/PlaceHeatwaves.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceHeatwaves : MonoBehaviour {
+
+    GameObject heatwave;
+    GameObject player;
+    // Use this for initialization
+    void Start () {
+        heatwave = (GameObject)Resources.Load("HeatWave");
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CreateHW() {
+        float RandValue = Random.value;
+        float PosZ = player.transform.position.z;
+        //float posX = Random.RandomRange();
+        if(RandValue >= 0 && RandValue <= 0.3) {
+           // Instantiate(heatwave, player.transform.position.) )
+        }
+    }
+}
