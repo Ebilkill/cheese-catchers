467c47ff5f6302ba9248ad78ba8853046b5f4133
Ebilkill <bartwijgers@hotmail.com>
2017-03-30 12:57:56 +0200

    Add improved heat bar + script :)
    Doesn't deplete or replenish, but it's there, taking values in range [0, 1]

---
+++ Cheese Catchers/Assets/Scripts/HeatBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatBar : MonoBehaviour {
+
+    // How filled the heat bar is, [0, 1]
+    public float heatBarFill;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.GetComponent<RectTransform>().sizeDelta = new Vector2(heatBarFill * 1050 - 525, GetComponent<RectTransform>().sizeDelta.y);
+	}
+}
